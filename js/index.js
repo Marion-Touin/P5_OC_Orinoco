a1d43fc9b827a9ba00c24b1c916745538da0ef4d
Marion Touin <t.marion@hotmail.com>
2020-07-31 16:14:31 +0200

    création boucle

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
+/*Récupération donnée vie l'api*/
 const getTeddies = async function() {
     let response = await fetch('http://localhost:3000/api/teddies')
     let data = await response.json()
     console.log(data)
-}
+    /*boucle pour récupérer la description*/
+    data.forEach(function(ourson){
+        console.log(ourson.imageUrl);
+        console.log(ourson.name);
+        console.log(ourson.price);
+        console.log(ourson._id);
+    });
+    /*Injection des produits dans le HTML vis JS*/
 
+}
 getTeddies()
 
