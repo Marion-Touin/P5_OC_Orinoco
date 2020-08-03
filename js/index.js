ba30f333d55a80eb8ff9e723af5cc9b8189d2ce9
Marion Touin <t.marion@hotmail.com>
2020-08-03 18:32:49 +0200

    affichage produits page d'accueil

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-/*Récupération donnée vie l'api*/
-const getTeddies = async function() {
-    let response = await fetch('http://localhost:3000/api/teddies')
-    let data = await response.json()
-    console.log(data)
-    /*boucle pour récupérer la description*/
-    data.forEach(function(ourson){
-        console.log(ourson.imageUrl);
-        console.log(ourson.name);
-        console.log(ourson.price);
-        console.log(ourson._id);
-    });
-    /*Injection des produits dans le HTML vis JS*/
+// Récupération des produits sur l'API
+fetch('http://localhost:3000/api/teddies')
+.then(response => response.json())
+.then(produits => {
+    //création d'une constante
+    const mesOursons = document.querySelector('#mesOursons');
+    //boucle pour récupérer les produits
+    for(let i = 0; i < produits.length; i++){
+        //insertion js dans html
+        mesOursons.innerHTML += `
+        <article class="col-md-10 col-lg-6">
+        <div class="card mb-5">
+        <img class="card-img" src="${produits[i].imageUrl}" alt="Ourson">
+        <h2 class="card-title text-center">${produits[i].name}</h2>
+        <p class="card-text text-center">${produits[i].price/100} €</p>
+        <a href="produit.html"><button class="btn btn-lg">Voir le produit</button></a>
+        </div>
+        </article>` 
+;
+    }
 
-}
-getTeddies()
+});
 
