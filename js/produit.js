3b2db134cf63ef645d4d36784af0629f30e8ac06
Marion Touin <t.marion@hotmail.com>
2020-08-12 15:54:18 +0200

    correction produit.js

--- js/produit.js
+++ js/produit.js
@@ -1,13 +1,10 @@
 //recuperer id
-let params = new URLSearchParams ('document.location.href');
-let url = params.get("_id");
-for (let p of params){
-    console.log(p[1]);
-}
-fetch('http://localhost:3000/api/teddies/'+url)
+let params = new URLSearchParams (document.location.href.split('?')[1]);
+let url = params.get("id_ourson");
+fetch('http://localhost:3000/api/teddies/'+ url)
 .then(response => response.json())
-.then(oursons => {
+.then(ourson => {
  
-   console.log('ourson[i].name');
+   console.log(ourson.name);
    
 }).catch(error => console.log(error))
