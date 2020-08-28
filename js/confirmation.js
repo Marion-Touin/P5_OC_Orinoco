02abbe2cf6c5a2d57dac749178a6502ebef2a3ac
Marion Touin <t.marion@hotmail.com>
2020-08-28 13:58:03 +0200

    design + responsive page confirmation

--- js/confirmation.js
+++ js/confirmation.js
@@ -12,22 +12,26 @@ for (i=0; i < commande.length; i++) {
 
     //création du h2
     let remerciement = document.createElement("h2");
+    remerciement.classList.add("message");
     remerciement.innerHTML = "Nous vous remerçions pour votre commande, elle sera traité dans les meilleures délais !";
     containerConfirmation.appendChild(remerciement);
 
     // création du h3
-    let recaptilatif = document.createElement("h3");
-    recaptilatif.innerHTML = "Voici le récapitulatif de votre commande !";
-    containerConfirmation.appendChild(recaptilatif);
+    let recapitulatif = document.createElement("h3");
+    recapitulatif.classList.add("recapitulatif");
+    recapitulatif.innerHTML = "Voici le récapitulatif de votre commande :";
+    containerConfirmation.appendChild(recapitulatif);
 
     //ajout du l'id commande
     let id = document.createElement("p");
+    id.classList.add("paragraphe");
     id.innerHTML = "Identifiant de la commande : "+ recapCommande.idCommande;
     containerConfirmation.appendChild(id);
 
     // Ajout du prix
     let prix = document.createElement("p");
-    prix.innerHTML = "prix total de votre commande : " + recapCommande.prixTotal + " €";
+    prix.classList.add("paragraphe");
+    prix.innerHTML = "Prix total de votre commande : " + recapCommande.prixTotal + " €";
     containerConfirmation.appendChild(prix);
 }
 localStorage.clear();
